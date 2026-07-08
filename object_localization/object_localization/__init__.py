0e2a5eb0f9c6af742b60c0cd0a05930c14e2449e
Petr Vanc <petr.vanc@cvut.cz>
2026-07-08 14:11:51 +0200

    .env variables

--- object_localization/object_localization/__init__.py
+++ object_localization/object_localization/__init__.py
@@ -1,4 +1,6 @@
 
 import os
 path = os.path.dirname(os.path.abspath(__file__))
-package_path = "/".join(path.split("/")[:-1])
+# OBJECT_LOCALIZATION_PATH points to the package source dir (holds cfg/ and
+# config/); without it the ROS2 build dir is used (see franka_hri README)
+package_path = os.path.expanduser(os.environ.get("OBJECT_LOCALIZATION_PATH", "/".join(path.split("/")[:-1])))
--- trajectory_data/trajectory_data/__init__.py
+++ trajectory_data/trajectory_data/__init__.py
@@ -1,4 +1,6 @@
 
 import os
 path = os.path.dirname(os.path.abspath(__file__))
-package_path = "/".join(path.split("/")[:-1])
+# TRAJECTORY_DATA_PATH points to the package source dir (holds trajectories/);
+# without it the ROS2 build dir is used (see franka_hri README)
+package_path = os.path.expanduser(os.environ.get("TRAJECTORY_DATA_PATH", "/".join(path.split("/")[:-1])))
